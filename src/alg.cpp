fb02fb03a3deb670ecf964d32777815e75807d92
ErrorDa <99288583+ErrorDa@users.noreply.github.com>
2022-05-30 00:17:42 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -16,7 +16,7 @@ BST<std::string> makeTree(const char* name) {
     std::string word = "";
     while (!file.eof()) {
         char IdontWantToDoIt = file.get();
-        int ch = int(IdontWantToDoIt);
+        int ch = IdontWantToDoIt;
         if ((ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122)) {
             word += tolower(ch);
         } else { if (word != "") {
