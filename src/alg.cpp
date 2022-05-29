b3d922a4402a8f680cfd9cfb2bd0a2706d0ae3c0
ErrorDa <99288583+ErrorDa@users.noreply.github.com>
2022-05-30 00:02:08 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,8 +3,31 @@
 #include  <fstream>
 #include  <locale>
 #include  <cstdlib>
+#include <string>
+using namespace std;
 #include  "bst.h"
 
-BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+BST<string> makeTree(const char* name) {
+      BST<string> tree;
+    ifstream file(name);
+    if (!file) {
+        cout << "Something wrong" <<"\n";
+        throw string("Can not open file!");
+    }
+    string word = "";
+    while (!file.eof()) {
+        int ch = int(file.get());
+        if ((ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122)) {
+            word += tolower(ch);
+        }
+        else {
+            if (word != "") {
+                tree.Add(word);
+                word = "";
+            }
+        }
+    }
+    file.close();
+    return tree;
+}
 }
